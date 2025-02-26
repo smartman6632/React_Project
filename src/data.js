c32a80e515320a435e66ae582d18f1f00e32eb7c
smartman6632 <165347738+smartman6632@users.noreply.github.com>
2025-02-26 16:28:34 +1100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const CONTENT_INFO={
+    island:{
+        h1:"Never Stop",
+        h2:"Explore the world",
+        p:"This is Yi Ren's React project, designing for demonstration",
+        src:"/assets/videos/island.mp4"
+    },
+    spring:{
+        h1:"Spring",
+        h2:"Feel the new vitality",
+        p:"This is Yi Ren's React project,designing for demonstration",
+        src:"/assets/videos/spring.mp4"
+    },
+    summer:{
+        h1:"Summer",
+        h2:"Enjoy passionate time",
+        p:"This is Yi Ren's React project,designing for demonstration",
+        src:"/assets/videos/summer.mp4"
+    },
+    autumn:{
+        h1:"Autumn",
+        h2:"Experience without limits",
+        p:"This is Yi Ren's React project,designing for demonstration",
+        src:"/assets/videos/autumn.mp4"
+    },
+    winter:{
+        h1:"Winter",
+        h2:"Enjoy peace and freedom",
+        p:"This is Yi Ren's React project,designing for demonstration",
+        src:"/assets/videos/winter.mp4"
+    }
+}
